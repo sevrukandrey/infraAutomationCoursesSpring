652accbfd0f20821c47cc4b22fca3d9ba527e085
Andrey Sevruk <asevruk@playtika.com>
2017-11-03 17:36:03 +0200

    2

--- src/main/java/com/playtika/automation/controller/CarsController.java
+++ src/main/java/com/playtika/automation/controller/CarsController.java
@@ -2,36 +2,41 @@ package com.playtika.automation.controller;
 
 import com.playtika.automation.domain.Car;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.http.MediaType.*;
 
 @Controller
+@RequestMapping(produces = APPLICATION_JSON_UTF8_VALUE)
 public class CarsController {
     List<Car> cars = new ArrayList<>();
 
-
     @PostMapping(value = "/addCar")
     @ResponseBody
-    public long addCar(@RequestBody Car car) {
-        cars.add(car);
-        return car.id;
+    public ResponseEntity addCar(@RequestBody Car car) throws JSONException {
+        JSONObject response = new JSONObject();
+        response.put("carId", "100500");
+
+        return new ResponseEntity(response.toString(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/getAllCars")
-    @ResponseBody
-    public String getAllCars() {
-        return cars.toString();
+    public List<Car> getAllCars() {
+        return cars;
     }
 
     @DeleteMapping(value = "/deleteCar")
     @ResponseBody
     public void deleteCar(@RequestParam("id") long id) {
-            cars.stream()
+
     }
 
     @GetMapping(value = "/getCarInfo")
--- src/main/java/com/playtika/automation/domain/Car.java
+++ src/main/java/com/playtika/automation/domain/Car.java
@@ -7,21 +7,13 @@ import lombok.Data;
 public class Car {
     public long id;
     public String brand;
-    public int year;
-    public String color;
-    public int price;
-    public boolean sold;
 
     public Car(){
 
     }
 
-    public Car(long id, String brand, int year, String color, int price, boolean sold) {
+    public Car(long id, String brand) {
         this.id = id;
         this.brand = brand;
-        this.year = year;
-        this.color = color;
-        this.price = price;
-        this.sold = sold;
     }
 }

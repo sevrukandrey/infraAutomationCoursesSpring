d74dc7f969b9d45dc08fd235c73f5836b1cb25e5
asevruk <sevrukandrey@gmail.com>
2017-11-13 22:42:24 +0200

    12

--- src/test/java/com/playtika/automation/web/CarControllerTest.java
+++ src/test/java/com/playtika/automation/web/CarControllerTest.java
@@ -2,6 +2,7 @@ package com.playtika.automation.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.playtika.automation.domain.Car;
+import com.playtika.automation.domain.CarForSale;
 import com.playtika.automation.service.CarService;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,11 +14,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Map;
+
 import static org.mockito.Matchers.anyDouble;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -33,27 +38,70 @@ public class CarControllerTest {
     @Before
     public void init() {
         mockMvc = MockMvcBuilders
-            .standaloneSetup(new CarsController(carService))
-            .build();
+                .standaloneSetup(new CarsController(carService))
+                .build();
     }
 
     @Test
-    public void name() throws Exception {
-        when(carService.addCar(anyObject(),anyDouble(),anyString())).thenReturn(1L);
+    public void addCarShouldGetAndResponseInJson() throws Exception {
+        when(carService.addCar(anyObject(), anyDouble(), anyString())).thenReturn(1L);
 
         mockMvc.perform(post("/car")
-            .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-            .content("{\"brand\": \"Ford\",\"model\":\"fiesta\"}")
-            .param("price", String.valueOf(1000))
-            .param("ownerContacts", "Amdre"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json;charset=UTF-8"));
-   }
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content("{\"brand\": \"Ford\",\"model\":\"fiesta\"}")
+                .param("price", String.valueOf(1000))
+                .param("ownerContacts", "Amdre"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json("1"));
+    }
 
     @Test
-    public void name2() throws Exception {
-        mockMvc.perform(post("car").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isBadRequest());
+    public void addCarShouldSendBadRequestOnMissingParams() throws Exception {
+        mockMvc.perform(post("/car").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void deleteCarShouldSendBadRequestOnMissingParams() throws Exception {
+        mockMvc.perform(delete("/car").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void getCarDetailsShouldSendBadRequestOnMissingParams() throws Exception {
+        mockMvc.perform(get("/car").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void getAllCarsShouldGetAndResponseInJson() throws Exception {
+
+        mockMvc.perform(get("/cars")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+    }
+
+    @Test
+    public void deleteCarsShouldGetAndResponseInJson() throws Exception {
+
+        mockMvc.perform(delete("/car")
+                .param("id", String.valueOf(1))
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+    }
+
+    @Test
+    public void getCarDetailsShouldGetAndResponseInJson() throws Exception {
+
+        mockMvc.perform(get("/car")
+                .param("id", String.valueOf(1))
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(""))
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
     }
 
 

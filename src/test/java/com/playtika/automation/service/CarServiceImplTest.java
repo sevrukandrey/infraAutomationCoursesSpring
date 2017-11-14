e2e2cc920fde5385afc83780653982160b2fb2d1
asevruk <sevrukandrey@gmail.com>
2017-11-14 13:45:45 +0200

    12

--- src/test/java/com/playtika/automation/service/CarServiceImplTest.java
+++ src/test/java/com/playtika/automation/service/CarServiceImplTest.java
@@ -1,9 +1,39 @@
 package com.playtika.automation.service;
 
+import com.playtika.automation.domain.Car;
+import com.playtika.automation.domain.CarForSale;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarServiceImplTest {
 
+    private CarService carService;
+
+    @Before
+    public void init() {
+        carService = new CarServiceImpl();
+    }
+
+    @Test
+    public void shouldReturnAllCars() {
+
+    }
+
+    private CarForSale getCarForSale() {
+        CarForSale carForSale  = new CarForSale();
+        carForSale.setId(1);
+        carForSale.setOwnerContacts("Andrey");
+        carForSale.setBrand("ford");
+        carForSale.setModel("fiesta");
+        carForSale.setPrice(100);
+
+    }
 }

69c8be420172bf5e3a39417e3e6c46fc6a08ddd0
Andrey Sevruk <asevruk@playtika.com>
2017-12-07 14:34:38 +0200

    move service to spring data

--- src/test/java/com/playtika/automation/dao/AdvertEntityRepositoryTest.java
+++ src/test/java/com/playtika/automation/dao/AdvertEntityRepositoryTest.java
@@ -1,12 +1,19 @@
 package com.playtika.automation.dao;
 
+import com.github.database.rider.core.DBUnitRule;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.playtika.automation.dao.entity.AdvertEntity;
 import com.playtika.automation.domain.AdvertStatus;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,22 +25,35 @@ public class AdvertEntityRepositoryTest {
     @Autowired
     AdvertEntityRepository advertEntityRepository;
 
+    @Autowired
+    protected JdbcTemplate jdbcTemplate;
+
+    @Rule
+    public DBUnitRule dbUnitRule = DBUnitRule.instance(() -> jdbcTemplate.getDataSource().getConnection());
+
+    @Autowired
+    protected TestEntityManager em;
+
+    @Autowired
+    protected AdvertEntityRepository dao;
+
 
     @Test
-    @DataSet("")
+    @DataSet("expected-advert.xml")
     public void shouldFindAdvertByStatus(){
-          assertThat(advertEntityRepository.findByStatus(AdvertStatus.OPEN)).hasSize(1);
+        List<AdvertEntity> byStatus = dao.findByStatus(AdvertStatus.OPEN);
+        assertThat(byStatus.get(0)).isNotNull();
     }
 
 
-    @Test
-    public void shouldFindAdvertByCarIdAndStatus(){
-
-    }
-    @Test
-    public void shouldDeleteByCarId(){
-
-    }
+//    @Test
+//    public void shouldFindAdvertByCarIdAndStatus(){
+//
+//    }
+//    @Test
+//    public void shouldDeleteByCarId(){
+//
+//    }
 
 
 
